982df66a87f622bc0eb78d7cb64a37c1f2a9d04f
txemasv <txemasv@gmail.com>
2017-12-23 12:44:49 +1100

    first commit

---
+++ src/main/java/org/txemasv/mycharacters/apiserver/App.java
@@ -0,0 +1,20 @@
+package org.txemasv.mycharacters.apiserver;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application running");
+
+	}
+
+}
