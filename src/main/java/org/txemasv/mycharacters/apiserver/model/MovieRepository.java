b18f26c59f0b5a3c160f88d51286adc9b23168ff
txemasv <txemasv@gmail.com>
2017-12-27 16:10:10 +1100

    remove movieRepository, is not necessary

--- src/main/java/org/txemasv/mycharacters/apiserver/model/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package org.txemasv.mycharacters.apiserver.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface MovieRepository extends MongoRepository<Movie, String> {
-
-}
