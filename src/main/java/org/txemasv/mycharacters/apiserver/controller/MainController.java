5cbfa65d5a67ede2ab351ea37075d879f75f920b
txemasv <txemasv@gmail.com>
2017-12-28 15:10:35 +1100

    @CrossOrigin annotation to work in localhost with Angular.

--- src/main/java/org/txemasv/mycharacters/apiserver/controller/MainController.java
+++ src/main/java/org/txemasv/mycharacters/apiserver/controller/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import org.txemasv.mycharacters.apiserver.util.ItemType;
 import org.txemasv.mycharacters.apiserver.util.ResponseType;
 import org.txemasv.mycharacters.apiserver.util.SuccessType;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class MainController {
 
